f58c39594ba66b79c9ff7e9a47c1f5cbd29b9351
Joshua Isom <110069928+JoshIsomLehigh@users.noreply.github.com>
2024-12-27 22:06:57 -0500

    Update test_adventure_5.py

--- test/test_adventure_5.py
+++ test/test_adventure_5.py
@@ -1,27 +1,11 @@
 import pytest
-from student_code import ShortestPathDAG
+from pylint.lint import Run
+import subprocess
+import adventure
 
-class TestShortestPathDAG:
-    @pytest.fixture
-    def setup_dag(self):
-        dag = ShortestPathDAG()
-        # Add nodes and edges
-        dag.add_node('A')
-        dag.add_node('B')
-        dag.add_node('C')
-        dag.add_node('D')
-        dag.add_node('E')
-        dag.add_edge('A', 'B', edge_weight=1)
-        dag.add_edge('B', 'C', edge_weight=2)
-        dag.add_edge('A', 'C', edge_weight=4)
-        dag.add_edge('C', 'D', edge_weight=3)
-        dag.add_edge('A', 'E', edge_weight=10)
-        return dag
+# Test Cases for Week 1 Adventure Game
 
- 
-
-    def test_no_path(self, setup_dag):
-        dag = setup_dag
-        dag.add_node('F')  # Adding an extra node 'F' that isn't connected
-        _, distance = dag.shortest_path('A', 'F')
-        assert distance == float('inf'), "There should be no path from A to F because node F is not connected"
+def test_string_manipulation():
+    """Test for correct string manipulation in intro."""
+    assert isinstance(adventure.intro, str), "intro should be a string"
+    assert adventure.character_name in adventure.intro, "intro should contain character_name"
