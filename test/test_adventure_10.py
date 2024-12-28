777cf948bea1c4eb2871efe4bc1c45d192d01066
Joshua Isom <110069928+JoshIsomLehigh@users.noreply.github.com>
2024-12-27 22:08:34 -0500

    Update test_adventure_10.py

--- test/test_adventure_10.py
+++ test/test_adventure_10.py
@@ -1,30 +1,12 @@
 import pytest
-from student_code import ShortestPathDAG
+from pylint.lint import Run
+import subprocess
+import adventure
 
-class TestShortestPathDAG:
-    @pytest.fixture
-    def setup_dag(self):
-        dag = ShortestPathDAG()
-        # Add nodes and edges
-        dag.add_node('A')
-        dag.add_node('B')
-        dag.add_node('C')
-        dag.add_node('D')
-        dag.add_node('E')
-        dag.add_edge('A', 'B', edge_weight=1)
-        dag.add_edge('B', 'C', edge_weight=2)
-        dag.add_edge('A', 'C', edge_weight=4)
-        dag.add_edge('C', 'D', edge_weight=3)
-        dag.add_edge('A', 'E', edge_weight=10)
-        return dag
-
-    def test_topological_sort(self, setup_dag):
-        dag = setup_dag
-        top_sort = dag.top_sort()
-        valid_sorts = [
-            ['A', 'B', 'C', 'D', 'E'],
-            ['A', 'B', 'C', 'E', 'D'],
-            ['A', 'E', 'B', 'C', 'D'],
-            ['A', 'E', 'B', 'D', 'C'],
-        ]
-        assert top_sort in valid_sorts, "Topological sort should be one of the valid topological orders"
+def test_file_exists():
+    """Test that adventure.py exists."""
+    try:
+        with open("adventure.py", "r") as f:
+            assert True
+    except FileNotFoundError:
+        assert False, "adventure.py not found"
